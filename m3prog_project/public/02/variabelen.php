3ed1cf56a2dda8c06060a4a7ee7202e27b8b7d6e
Julnar <36888@ma-web.nl>
2024-03-05 15:20:48 +0100

    02variabelen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,17 @@
+<?php
+$auto2 = "Nissan";
+$_naam = "Julnar";
+//$auto's = "" niet geldig.
+//$auto/onderdelen = "" niet geldig.
+//$huis&tuin = "" niet geldig.
+$naw = "rawr";
+//$22 = niet geldig.
+//$$auto = "Lambo";
+$kleuVanAuto = "Rood";
+//$software-dev = niet geldig.
+
+echo "$auto2";
+echo "<br>$_naam";
+echo "<br>$naw";
+echo "<br>$kleuVanAuto";
+?>
