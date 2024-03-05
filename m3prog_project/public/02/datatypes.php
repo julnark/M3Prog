86ee8c27308f4e00112bd2cab97f92110ec9def5
Julnar <36888@ma-web.nl>
2024-03-05 14:54:22 +0100

    01

---
+++ m3prog_project/public/02/datatypes.php
@@ -0,0 +1,13 @@
+<?php
+$huisnumer = 11;
+echo "Ik woon in huisnummer $huisnumer";
+
+$isGeslaagd = "gezakt";
+echo "<br>Je bent $isGeslaagd";
+
+$prijs = 1.9;
+echo "<br>De prijs voor een liter benzine is $prijs";
+
+$boolean = "false";
+echo "<br> Ik heb een broertje $boolean";
+?>
