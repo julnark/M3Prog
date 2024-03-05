a1a886a84e369e1c72c5d2d9b027cac16b466b57
Julnar <36888@ma-web.nl>
2024-03-05 14:25:36 +0100

    04

---
+++ m3prog_project/public/01/variabelen.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $naam = "Julnar";
+    $woonplaats = "Castricum";
+    $leeftijd = 18;
+    $temperatuur = 8.5;
+    echo "Ik ben $naam, ik ben $leeftijd jaar oud en ik woon in $woonplaats. De temperatuur is nu $temperatuur graden, "
+    ?>
+</body>
+</html>
