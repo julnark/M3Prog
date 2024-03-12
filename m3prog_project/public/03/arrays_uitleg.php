d81cd5b53408085f76d999fb21a4e28f3b79c4ee
Julnar <36888@ma-web.nl>
2024-03-12 14:53:23 +0100

    01arraysuitleg

---
+++ m3prog_project/public/03/arrays_uitleg.php
@@ -0,0 +1,21 @@
+<?php
+$games = ["Rocket league","Phasmo","GTA","roblox","fortnite"];
+print_r($games);
+echo "<br>";
+echo $games[3];
+echo "<br>";
+
+print_r($games);
+echo "<br>";
+var_dump($games);
+
+echo "<br>";
+echo count($games);
+
+echo "<br>";
+array_push($games, "Kat cafe");
+print_r($games);
+
+echo "<br>";
+sort($games);
+echo implode(" ", $games);
