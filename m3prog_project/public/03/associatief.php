3df4dd1cf4ee260aef6ec1f2df7eb39f7592d16f
Julnar <36888@ma-web.nl>
2024-03-22 09:44:45 +0100

    05foreach-loop

--- m3prog_project/public/03/associatief.php
+++ m3prog_project/public/03/associatief.php
@@ -1,3 +1,35 @@
 <?php 
 $verbruik = ['Wasdroger' => 230, 'Vaatwasser' => 250, 'stofzuiger' => 80 ,'Kookplaat' => 180, 'Stoomcabine' => 180];
+
+foreach($verbruik as $apparaat => $verbruik){
+    echo $apparaat . "=" . $verbruik . "<br/>";
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associatief</title>
+</head>
+<body>
+    <?php
+    $verbruik = ['Wasdroger' => 230, 'Vaatwasser' => 250, 'stofzuiger' => 80 ,'Kookplaat' => 180, 'Stoomcabine' => 180];
+foreach ($verbruik as $apparaat => $verbruik_waarde) {
+    echo "<h2>Naam van het apparaat: " . $apparaat . "</h2>";
+    echo "<p>Verbruik: " . $verbruik_waarde . " watt</p>";
+};
+$totaal = 0; 
+
+foreach ($verbruik as $apparaat => $verbruik_waarde) {
+    $totaal += $verbruik_waarde;
+}
+
+echo "<h2>Totaal verbruik: </h2>" . $totaal;
+
+
+
+?>
+</body>
+</html>
