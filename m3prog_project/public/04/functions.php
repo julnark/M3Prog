adb2e975d34fab863322ea3fced9650a3e19f7a9
Julnar <36888@ma-web.nl>
2024-03-26 21:25:36 +0100

    function-coden

--- m3prog_project/public/04/functions.php
+++ m3prog_project/public/04/functions.php
@@ -36,3 +36,16 @@ function berekenRitkosten($afstand_km, $km_per_liter, $liter_prijs)
     $kosten_euro = $aantal_liters * $liter_prijs;
     return $kosten_euro;
 };
+
+function berekenVliegkosten($afstandkm, $kerosine_prijs, $kilo_bagage, $class)
+{
+    $kosten_reis = ($kerosine_prijs * $afstandkm) / 30;
+    $prijs_bagage = ($kilo_bagage * 5);
+    $kosten_totaal = $kosten_reis + $prijs_bagage;
+
+    if ($class === 'business') {
+        $kosten_totaal = $kosten_totaal * 1.5;
+    };
+
+    return $kosten_totaal;
+};
